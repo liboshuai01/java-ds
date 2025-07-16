f7eb960be14842ed732ccb2a952fbb36f9d06680
Boshuai Li <liboshuai01@gmail.com>
2025-07-16 23:43:11 +0800

    docs(binarysearch): 更新二分查找算法注释
    - 在 basic 和 improved 两个方法的注释中添加了时间复杂度和空间复杂度说明
    - 在 basic 方法注释中添加了二分查找算法原理和溢出问题相关知识点- 在 improved 方法注释中添加了关于 j = a.length 时的实现细节知识点

--- src/main/java/cn/liboshuai/learning/binarysearch/BinarySearch.java
+++ src/main/java/cn/liboshuai/learning/binarysearch/BinarySearch.java
@@ -7,6 +7,13 @@ public class BinarySearch {
      * @param a 被查找的升序数组
      * @param target 需要被找到的目标元素值
      * @return  目标元素值的数组索引下标（如果没有找到元素，则索引下标返回-1）
+     * 时间复杂度：O(logN)
+     * 空间复杂度：O(1)
+     * 知识点：
+     * 1. 二分查找算法的原理
+     * 2. 求中间数使用(j+i)/2时，int数据类型溢出，导致出现负数问题
+     *  - 1. 原码、反码、补码
+     *  - 2. 简单数学公式转换，将(j+i)/2转为i+(j-i)/2
      */
     public static int binarySearchBasic(int[] a, int target) {
         int i = 0;
@@ -29,6 +36,10 @@ public class BinarySearch {
      * @param a 被查找的升序数组
      * @param target 需要被找到的目标元素值
      * @return  目标元素值的数组索引下标（如果没有找到元素，则索引下标返回-1）
+     * 时间复杂度：O(logN)
+     * 空间复杂度：O(1)
+     * 知识点：
+     * 1. 当 j = a.length时，下面的循环条件如何更改，j值如何更改
      */
     public static int binarySearchImproved(int[] a, int target) {
         int i = 0;
