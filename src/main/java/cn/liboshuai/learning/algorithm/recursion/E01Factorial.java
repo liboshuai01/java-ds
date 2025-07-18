413a16b5a54c8e4c6ae9acb11d3fac89253c1874
Boshuai Li <liboshuai01@gmail.com>
2025-07-18 16:13:38 +0800

    add E01Factorial

--- src/main/java/cn/liboshuai/learning/algorithm/recursion/E01Factorial.java
+++ src/main/java/cn/liboshuai/learning/algorithm/recursion/E01Factorial.java
@@ -2,11 +2,15 @@ package cn.liboshuai.learning.algorithm.recursion;
 
 public class E01Factorial {
     public static void main(String[] args) {
-
+        f(5);
     }
 
-    public static int f(int n) {
+    public static void f(int n) {
+        if (n == 0) {
+            return;
+        }
+        System.out.println("倒序打印: n = " + n);
         f(n - 1);
-
+        System.out.println("正序打印: n = " + n);
     }
 }
